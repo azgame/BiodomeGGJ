6df4791ee98dd7ac2137d307349eeb5935606976
Robert <robert.blank.wardo@gmail.com>
2020-02-01 10:37:18 -0500

    turret with UI

--- BiodomeGGJ/Assets/Scripts/Enemy.cs
+++ BiodomeGGJ/Assets/Scripts/Enemy.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class Enemy : MonoBehaviour
+public abstract class Enemy : MonoBehaviour
 {
 
     public Rigidbody rb;
+    public Transform goal;
+    
+    
     // Start is called before the first frame update
     void Start(){
         rb = GetComponent<Rigidbody>();
-        
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
