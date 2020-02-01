43a64b9927d2fffbeeee18b26c368c91ba104088
Robert <robert.blank.wardo@gmail.com>
2020-02-01 15:51:49 -0500

    added tower range change

---
+++ BiodomeGGJ/Assets/Scripts/TowerDetection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerDetection : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+
+            this.gameObject.GetComponentInParent<Tower>().myEnemies.Add(other.gameObject);
+        }
+    }
+
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy" && this.gameObject.GetComponentInParent<Tower>().myEnemies.Contains(other.gameObject))
+        {
+
+            this.gameObject.GetComponentInParent<Tower>().myEnemies.Remove(other.gameObject);
+        }
+    }
+}
