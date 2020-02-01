debd4057e267705e5f7dd310aac9c0e892646f75
azgame <azizysgame@gmail.com>
2020-01-31 22:52:52 -0500

    pre pull

--- BiodomeGGJ/Assets/Scripts/Player.cs
+++ BiodomeGGJ/Assets/Scripts/Player.cs
@@ -25,45 +25,13 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Input
+        // Movement
         m_move.Move(m_moveDir);
     }
 
+    // Move Action
     public void Move(InputAction.CallbackContext context)
     {
         m_moveDir = context.ReadValue<Vector2>();
     }
 }
-
-
-
-//Vector2 GetInputXY(int actorId_)
-//{
-//    if (m_gamepad != null)
-//    {
-//        return m_gamepad.leftStick.ReadValue();
-//    }
-//    else
-//    {
-//        Vector2 moveDir = new Vector2();
-//        switch (actorId_)
-//        {
-//            case 1:
-
-//                float horizontal = m_keyboard.dKey.ReadValue() - m_keyboard.aKey.ReadValue();
-//                float vertical = m_keyboard.wKey.ReadValue() - m_keyboard.sKey.ReadValue();
-//                moveDir = new Vector2(horizontal, vertical);
-//                break;
-//            case 2:
-//                break;
-//            case 3:
-//                break;
-//            case 4:
-//                break;
-//            default:
-//                break;
-//        }
-
-//        return moveDir;
-//    }
-//}
