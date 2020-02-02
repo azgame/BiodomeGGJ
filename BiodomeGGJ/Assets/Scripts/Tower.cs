fe474ca739cbda465a26d4248999b07dbfa49887
Mike Chong <mcchong@uwaterloo.ca>
2020-02-02 00:23:51 -0500

    Quick fix

--- BiodomeGGJ/Assets/Scripts/Tower.cs
+++ BiodomeGGJ/Assets/Scripts/Tower.cs
@@ -80,13 +80,6 @@ public abstract class Tower : MonoBehaviour, IInteractable
     
     virtual protected void Update()
     {
-        
-        if (myEnemies.Count != 0)
-        {
-        this.gameObject.transform.LookAt(myEnemies[0].transform);
-        transform.LookAt(new Vector3(myEnemies[0].transform.position.x, transform.position.y, myEnemies[0].transform.position.z));
-        attack();
-        }
 
         if (currentAmmo <= 0 && this.fillCount() > 2)
         {
